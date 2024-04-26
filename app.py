959a5bc256ed2f2240685ba996c4088256ca5c7d
Magno <magno.luque@upch.pe>
2024-04-25 21:23:48 -0500

    changes

--- app.py
+++ app.py
@@ -63,28 +63,36 @@ if clicked_node:
 st.pyplot(plt)
 
 ###########################
+asigCodAcro = {}
+asigAcroCod = {}
+nombresNivel = {}
+cursosNivel = {}
 
-niveles = {'PRIMER CICLO': ['F', 'CR1', 'CDI', 'AMGA', 'QG', 'III'],
- 'SEGUNDO CICLO': ['CR2', 'CII', 'FI1', 'BI', 'PII', 'FP'],
- 'TERCER CICLO': ['CVI', 'FI2', 'QO/QCS', 'CS', 'POO', 'EsD'],
- 'CUARTO CICLO': ['EcD', 'PA', 'FI3', 'EP', 'OAC', 'AED'],
- 'QUINTO CICLO': ['FD', 'I1', 'ADA', 'SO', 'CDR', 'IML'],
- 'SEXTO CICLO': ['PI1', 'BD', 'ILP', 'CSI', 'SI', 'AE1'],
- 'SÉTIMO CICLO': ['DP1', 'I2', 'HCD', 'CPD', 'IS', 'AE2'],
- 'OCTAVO CICLO': ['PI2', 'DP2', 'I3', 'DSW', 'V', 'AE3'],
- 'NOVENO CICLO': ['DNI', 'E', 'T1', 'I4', 'DSM', 'AE4'],
- 'DÉCIMO CICLO': ['NRI', 'T2', 'TASI', 'DI', 'AE5', 'AE6']}
+for index, row in df.iterrows():
+    asigCodAcro[row['Código']] = row['Acrónimo']
+
+for index, row in df.iterrows():
+    asigAcroCod[row['Acrónimo']] = row['Código']
+
+nivel = ["PRIMER","SEGUNDO","TERCER","CUARTO","QUINTO","SEXTO","SÉTIMO","OCTAVO","NOVENO","DÉCIMO"]
+
+for num, nombre in enumerate(nivel):
+  nombresNivel[nivel[num]] = num+1
+
+for nombre, ciclo in nombresNivel.items():
+    dicTem = [] 
+    for index, row in df.iterrows():
+        if ciclo == row['Ciclo']:
+            dicTem.append(row['Acrónimo'])
+    cursosNivel[nombre + ' CICLO'] = dicTem 
 
 
-level_labels = {"Nivel 2": ['A', 'B', 'C'], "Nivel 1": [1, 2, 3, 4], "Nivel 3":['D','E']}
-level_positions = {}
-for level, nodes in level_labels.items():
-    y_pos = sum([pos[node][1] for node in nodes]) / len(nodes)  # Calcula la posición Y promedio de los nodos en el nivel
-    level_positions[level] = (0.5, y_pos)
+posicionNivel = {}
+for nivel, nodos in cursosNivel.items():
+    y_pos = sum([pos[node][1] for node in nodos]) / len(nodos)  # Calcula la posición Y promedio de los nodos en el nivel
+    posicionNivel[nivel] = (0.5, y_pos)
 
-for level, position in level_positions.items():
-    plt.text(position[0], position[1], level, rotation=90, fontsize=12, verticalalignment='center', horizontalalignment='center')
+for nivel, posicion in posicionNivel.items():
+    plt.text(posicion[0], posicion[1], nivel, rotation=90, fontsize=12, verticalalignment='center', horizontalalignment='center')
 
-
-# Mostrar el grafo en Streamlit
 st.pyplot(plt)
